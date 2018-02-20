8320c5c61784a8e39bbba968810a71b90eda8335
Zomeren <115698@portal.drenthecollege.nl>
2018-02-20 11:28:42 +0100

    variables updated

--- app/src/main/java/com/example/patrick/helloworld/MainActivity.java
+++ app/src/main/java/com/example/patrick/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     int clickValue= 1;
     boolean item1 = false;
     TextView tv_clicks;
-    Button b_clickbutton, b_buybutton;
+    Button b_clickButton, b_buyButton;
+    ProgressBar pb_progressCircle;
 
 
 
@@ -22,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv_clicks = (TextView) findViewById(R.id.clicks);
-        b_clickbutton = (Button) findViewById(R.id.clickbutton);
-        b_buybutton = (Button) findViewById(R.id.buybutton);
-        b_buybutton.setEnabled(false);
+        b_clickButton = (Button) findViewById(R.id.clickButton);
+        b_buyButton = (Button) findViewById(R.id.buyButton);
+        pb_progressCircle =(ProgressBar) findViewById(R.id.progressCircle);
+        b_buyButton.setEnabled(false);
 
-        b_clickbutton.setOnClickListener(new View.OnClickListener() {
+        b_clickButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clickCounter = clickCounter + clickValue;
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if(clickCounter >= 10){
                     if(!item1) {
-                        b_buybutton.setEnabled(true);
+                        b_buyButton.setEnabled(true);
                         item1 = true;
 
                     }
@@ -42,14 +45,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        b_buybutton.setOnClickListener(new View.OnClickListener(){
+        b_buyButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 clickValue = clickValue + 2;
                 clickCounter = clickCounter - 10;
                 tv_clicks.setText("Clicks: " + clickCounter);
-                b_buybutton.setText("Max upgrade");
-                b_buybutton.setEnabled(false);
+                b_buyButton.setText("Max upgrade");
+                b_buyButton.setEnabled(false);
             }
         });
     }
